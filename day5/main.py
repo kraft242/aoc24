1d3c068743b30ef26a5806d72c30b249aa46318e
Karl Törnblom Bartholf <karltb@kth.se>
2024-12-05 10:48:10 +0100

    Refactor Day 5

--- day5/main.py
+++ day5/main.py
@@ -1,34 +1,11 @@
 from aocd import get_data
 from functools import cmp_to_key
-from itertools import pairwise
 
 
 def create_ordering(rules):
     parts = [r.split("|") for r in rules.splitlines()]
     parts = [(int(l), int(r)) for l, r in parts]
-    ordering = {l: [] for l, _ in parts}
-    for l, r in parts:
-        ordering[l].append(r)
-    return ordering
-    values = [(int(lhs), int(rhs)) for lhs, rhs in parts]
-
-    lt = {lhs: rhs for lhs, rhs in values}
-
-    ordering = list(set(v for pair in values for v in pair))
-
-    l = len(ordering)
-
-    for i in range(l - 1):
-        j_min = i
-        for j in range(i + 1,  l):
-            if lt.get(ordering[j], -1) == ordering[j_min]:
-                j_min = j
-        if j_min != i:
-            ordering[i], ordering[j_min] = ordering[j_min], ordering[i]
-
-    print(ordering)
-
-    print(values)
+    return set(parts)
 
 
 def parse_update(u):
@@ -39,95 +16,50 @@ def parse_updates(updates):
     return [parse_update(u) for u in updates.splitlines()]
 
 
-def update_is_valid(update, ordering):
-    size = len(update)
-    for i in range(size):
-        l = update[i]
-        for j in range(i + 1, size):
-            r = update[j]
-            if r in ordering and l in ordering[r]:
-                print(f"Update {update} is invalid")
-                return False
-    return True
+def parse_data(data):
+    rules, updates = data.split("\n\n")
+    return create_ordering(rules), parse_updates(updates)
+
+
+def sort_updates(updates, ordering):
+    def compare(l, r):
+        if (l, r) in ordering:
+            return -1
+        if (r, l) in ordering:
+            return 1
+        return 0
+
+    return [sorted(u, key=cmp_to_key(compare)) for u in updates]
+
+
+def midpoint(v):
+    return v[len(v) // 2]
 
 
 def part_one(data):
-    rules, updates = data.split("\n\n")
-    ordering = create_ordering(rules)
-    print(ordering)
+    ordering, updates = parse_data(data)
 
-    updates = parse_updates(updates)
+    ordered = sort_updates(updates, ordering)
 
-    result = 0
-    for u in updates:
-        if update_is_valid(u, ordering):
-            print(f"Update {u} is valid")
-            result += u[len(u) // 2]
-    return result
+    valid = [u for u, o in zip(updates, ordered) if u == o]
 
-    return 0
-
-
-def fix_invalid(update, ordering):
-    size = len(update)
-    for i in range(size):
-        l = update[i]
-        for j in range(i + 1, size):
-            r = update[j]
-            if r in ordering and l in ordering[r]:
-                update[i], update[j] = update[j], update[i]
-    return update
+    return sum(map(midpoint, valid))
 
 
 def part_two(data):
-    rules, updates = data.split("\n\n")
-    ordering = create_ordering(rules)
-    print(ordering)
+    ordering, updates = parse_data(data)
 
-    updates = parse_updates(updates)
+    ordered = sort_updates(updates, ordering)
 
-    invalid = []
-    for u in updates:
-        if not update_is_valid(u, ordering):
-            invalid.append(u)
+    invalid = [u for u, o in zip(updates, ordered) if u != o]
 
-    for update in invalid:
-        while not update_is_valid(update, ordering):
-            update = fix_invalid(update, ordering)
+    fixed = sort_updates(invalid, ordering)
 
-    return sum(u[len(u) // 2] for u in invalid)
+    return sum(map(midpoint, fixed))
 
 
 def main():
     data = get_data(day=5, year=2024)
-#    data = """47|53
-#97|13
-#97|61
-#97|47
-#75|29
-#61|13
-#75|53
-#29|13
-#97|29
-#53|29
-#61|53
-#97|53
-#61|29
-#47|13
-#75|47
-#97|75
-#47|61
-#75|61
-#47|29
-#75|13
-#53|13
-#
-#75,47,61,53,29
-#97,61,53,29,13
-#75,29,13
-#75,97,47,61,53
-#61,13,29
-#97,13,75,29,47"""
     one = part_one(data)
     two = part_two(data)
     print(f"Part one: {one}")
