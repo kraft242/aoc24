abd6d9bf0765e1b44810503fe2f7827bc02f988d
Karl Törnblom Bartholf <karltb@kth.se>
2024-12-13 10:34:57 +0100

    Refactor Day 13

--- day13/main.py
+++ day13/main.py
@@ -1,12 +1,13 @@
 from aocd import get_data
 import numpy as np
+from time import perf_counter_ns
 import re
 
 
 def parse_machine(machine):
     number_pattern = r"\d+"
     matches = re.finditer(number_pattern, machine, flags=re.DOTALL)
-    numbers = [int(m.group()) for m in matches]
+    numbers = (int(m.group()) for m in matches)
     ax, ay, bx, by, X, Y = numbers
     lhs = np.array([[ax, bx], [ay, by]])
     rhs = np.array([[X], [Y]])
@@ -20,20 +21,36 @@ def solve_system(lhs, rhs):
     return np.linalg.solve(lhs, rhs)
 
 
-def is_valid_solution(sol, atol=1e-3):
+def is_valid_solution(sol):
+    tolerance = 1e-3
     positive = np.all(sol >= 0)
     abs_diff = np.abs(sol - np.round(sol))
-    return positive and all(abs_diff < atol)
+    return positive and np.all(abs_diff < tolerance)
 
 
 def solve(data, increment=0):
     machines = data.split("\n\n")
-    token_cost = np.array([[3], [1]]).flatten()
+    token_cost = np.array([3, 1])
 
-    parsed = (parse_machine(m) for m in machines)
-    solutions = (solve_system(lhs, rhs + increment) for lhs, rhs in parsed)
-    valid_solutions = filter(is_valid_solution, solutions)
-    costs = (np.dot(sol.flatten(), token_cost) for sol in valid_solutions)
+    parsed = [parse_machine(m) for m in machines]
+    lhs_matrices = np.array([lhs for lhs, _ in parsed])
+    rhs_matrices = increment + np.array([rhs for _, rhs in parsed])
+
+    solutions = np.array(
+        [
+            solve_system(lhs, rhs)
+            for lhs, rhs in zip(lhs_matrices, rhs_matrices)
+        ]
+    )
+    valid_solutions = np.array(
+        [
+            sol
+            for sol in solutions
+            if is_valid_solution(sol)
+        ]
+    )
+
+    costs = np.dot(valid_solutions.reshape(-1, 2), token_cost)
 
     return int(sum(costs))
 
@@ -48,8 +65,11 @@ def part_two(data):
 
 def main():
     data = get_data(day=13, year=2024)
+    t0 = perf_counter_ns()
     one = part_one(data)
     two = part_two(data)
+    t1 = perf_counter_ns()
+    print(f"Time: {(t1 - t0) / 1e6:.2f}ms")
     print(f"Part one: {one}")
     print(f"Part two: {two}")
 
