58e7c4b4596b0aa36e519c50537d154ce579d43c
Karl Törnblom Bartholf <karltb@kth.se>
2025-01-07 21:21:11 +0100

    Solve Day 12 part 1

---
+++ day12/main.py
@@ -0,0 +1,103 @@
+from aocd import get_data
+import numpy as np
+
+delta = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+
+def parse_data(data):
+    return [list(row) for row in data.split("\n")]
+
+
+def in_bounds(y, x, height, width):
+    return 0 <= y < height and 0 <= x < width
+
+
+def on_bound(y, x, height, width):
+    return (y == 0) + (y == height - 1) + (x == 0) + (x == width - 1)
+
+
+def is_different(y, x, c, data):
+    if not in_bounds(y, x, len(data), len(data[0])):
+        return False
+    return data[y][x] != c
+
+
+def flood_fill(p, shape, data, visited):
+    y, x = p
+    Q = [(y, x)]
+    c = data[y][x]
+    height, width = len(data), len(data[0])
+    while Q:
+        y, x = Q.pop()
+        if in_bounds(y, x, height, width) and not visited[y, x] and data[y][x] == c:
+            visited[y, x] = True
+            shape.add((y, x))
+            Q.extend((y + dy, x + dx) for dy, dx in delta)
+
+
+def get_shapes(data):
+    height, width = len(data), len(data[0])
+
+    visited = np.zeros((height, width), dtype=np.bool)
+
+    shapes = []
+    for y in range(height):
+        for x in range(width):
+            if visited[y, x]:
+                continue
+            shape = set()
+            flood_fill((y, x), shape, data, visited)
+            shapes.append(shape)
+
+    return shapes
+
+
+def get_perimeters(shapes):
+    return [
+        sum(
+            (y + dy, x + dx) not in shape
+            for y, x in shape
+            for dy, dx in delta
+        )
+        for shape in shapes
+    ]
+
+
+def part_one(data):
+    data = parse_data(data)
+
+    shapes = get_shapes(data)
+
+    areas = [len(shape) for shape in shapes]
+
+    perimeters = get_perimeters(shapes)
+
+    areas = [int(a) for a in areas]
+    perimeters = [int(p) for p in perimeters]
+
+    return sum(a * p for a, p in zip(areas, perimeters))
+
+
+def part_two(data):
+    return 8
+
+def main():
+    # data = get_data(day=12, year=2024)
+    data = """RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"""
+    one = part_one(data)
+    two = part_two(data)
+    print(f"Part one: {one}")
+    print(f"Part two: {two}")
+
+
+if __name__ == "__main__":
+    main()
