a5ca1cc3f475cd3fa4e1c1bd16ed8249d64cbb0a
Karl Törnblom Bartholf <karltb@kth.se>
2025-01-07 18:22:24 +0100

    Solve Day 24 both parts

--- day24/main.py
+++ day24/main.py
@@ -1,21 +1,23 @@
 from aocd import get_data
 from time import perf_counter_ns
+import networkx as nx
+import matplotlib.pyplot as plt
+from graphviz import Digraph
+import re
 
 
 def parse_data(data):
-    active = {}
     initial, combined = data.split("\n\n")
-    active = {
-        wire: bool(int(value))
-        for line in initial.split("\n")
-        for wire, value in [line.split(": ")]
-    }
-    inactive = {
-        (l, op, r, rhs)
-        for line in combined.split("\n")
-        for lhs, rhs in [line.split(" -> ")]
-        for l, op, r in [lhs.split(" ")]
-    }
+    active, inactive = {}, set()
+    for line in initial.split("\n"):
+        wire, value = line.split(": ")
+        active[wire] = bool(int(value))
+
+    for line in combined.split("\n"):
+        lhs, rhs = line.split(" -> ")
+        l, op, r = lhs.split(" ")
+        inactive.add((l, op, r, rhs))
+
     return active, inactive
 
 
@@ -64,70 +66,53 @@ def part_one(data):
     return get_value(active, "z")
 
 
+def is_output(w):
+    return w.startswith("z")
+
+
+def is_input(w):
+    return w.startswith("x") or w.startswith("y")
+
+
 def part_two(data):
-    active, inactive = parse_data(data)
+    _, inactive = parse_data(data)
 
-    active = solve(active, inactive)
+    def rule_exists(op, input):
+        return any(
+            o == op and (l == input or r == input) for l, o, r, _ in inactive
+        )
 
-    x = get_value(active, "x")
-    y = get_value(active, "y")
-    z = get_value(active, "z")
+    def is_wrong(l, op, r, out):
+        both_inputs = is_input(l) and is_input(r)
 
-    print(f"Inputs: x={x}, y={y}")
-    print(f"Outputs: actual={z}, expected={x + y}, difference={(x + y) ^ z:b}")
+        valid_output = (
+            is_output(out)
+            and op != "XOR"
+            and out[1:] != "45"
+        )
+        valid_intermediate = (
+            op == "XOR"
+            and not is_output(out)
+            and not both_inputs
+        )
+        valid_inputs = (
+            both_inputs
+            and not (l[1:] == "00" and r[1:] == "00")
+            and (
+                (op == "XOR" and not rule_exists("XOR", out)) or
+                (op == "AND" and not rule_exists("OR", out))
+            )
+        )
 
-    return 0
+        return valid_output or valid_intermediate or valid_inputs
+
+    wrong = {rule for rule in inactive if is_wrong(*rule)}
+    outputs = {out for _, _, _, out in wrong}
+    return ",".join(sorted(outputs))
 
 
 def main():
     data = get_data(day=24, year=2024)
-#    data = """x00: 1
-# x01: 0
-# x02: 1
-# x03: 1
-# x04: 0
-# y00: 1
-# y01: 1
-# y02: 1
-# y03: 1
-# y04: 1
-#
-# ntg XOR fgs -> mjb
-# y02 OR x01 -> tnw
-# kwq OR kpj -> z05
-# x00 OR x03 -> fst
-# tgd XOR rvg -> z01
-# vdt OR tnw -> bfw
-# bfw AND frj -> z10
-# ffh OR nrd -> bqk
-# y00 AND y03 -> djm
-# y03 OR y00 -> psh
-# bqk OR frj -> z08
-# tnw OR fst -> frj
-# gnj AND tgd -> z11
-# bfw XOR mjb -> z00
-# x03 OR x00 -> vdt
-# gnj AND wpb -> z02
-# x04 AND y00 -> kjc
-# djm OR pbm -> qhw
-# nrd AND vdt -> hwm
-# kjc AND fst -> rvg
-# y04 OR y02 -> fgs
-# y01 AND x02 -> pbm
-# ntg OR kjc -> kwq
-# psh XOR fgs -> tgd
-# qhw XOR tgd -> z09
-# pbm OR djm -> kpj
-# x03 XOR y03 -> ffh
-# x00 XOR y04 -> ntg
-# bfw OR bqk -> z06
-# nrd XOR fgs -> wpb
-# frj XOR qhw -> z04
-# bqk OR frj -> z07
-# y03 OR x01 -> nrd
-# hwm AND bqk -> z03
-# tgd XOR rvg -> z12
-# tnw OR pbm -> gnj"""
     t0 = perf_counter_ns()
     one = part_one(data)
     two = part_two(data)
