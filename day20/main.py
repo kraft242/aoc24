7bdb58b615eede5ba46cb94d470e33c393e058c3
Karl Törnblom Bartholf <karltb@kth.se>
2025-01-10 13:06:01 +0100

    Solve Day 20 part 2

--- day20/main.py
+++ day20/main.py
@@ -1,6 +1,9 @@
 from aocd import get_data
 from collections import Counter
+from itertools import combinations
+from scipy.spatial import KDTree
 import networkx as nx
+import multiprocessing as mp
 
 DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
@@ -13,14 +16,13 @@ def parse_data(data):
         for x, c in enumerate(line)
         if c != "#"
     }
-    start = next(node for node in nodes if lines[node[1]][node[0]] == "S")
     end = next(node for node in nodes if lines[node[1]][node[0]] == "E")
 
-    return nodes, start, end
+    return nodes, end
 
 
 def build_graph(data):
-    nodes, start, end = parse_data(data)
+    nodes, end = parse_data(data)
     G = nx.Graph()
 
     for node in nodes:
@@ -30,7 +32,7 @@ def build_graph(data):
             if new_node in nodes:
                 G.add_edge(node, new_node)
 
-    return G, start, end
+    return G, end
 
 
 def get_width_height(data):
@@ -38,93 +40,69 @@ def get_width_height(data):
     return len(lines[0]), len(lines)
 
 
-def get_possible_cheats(data):
-    m = [list(line) for line in data.splitlines()]
+def manhattan_distance(l, r):
+    lx, ly = l
+    rx, ry = r
+    return abs(lx - rx) + abs(ly - ry)
 
+
+def time_saved(distance_to_end, cheat):
+    src, dst = cheat
+
+    return distance_to_end[src] - distance_to_end[dst] - manhattan_distance(src, dst)
+
+
+def get_cheats(G, max_distance):
+    nodes = sorted(G.nodes)
+    tree = KDTree(nodes)
     cheats = set()
+    min_distance = 2
 
-    # Single wall
-    for y, line in enumerate(m[1:-1], 1):
-        for x, c in enumerate(line[1:-1], 1):
-            # Horizontal
-            if m[y][x - 1] != "#" and m[y][x] == "#" and m[y][x + 1] != "#":
-                cheats.add(((x, y), (x + 1, y)))
-                cheats.add(((x - 1, y), (x, y)))
-            # Vertical
-            if m[y - 1][x] != "#" and m[y][x] == "#" and m[y + 1][x] != "#":
-                cheats.add(((x, y), (x, y + 1)))
-                cheats.add(((x, y - 1), (x, y)))
+    for node in nodes:
+        indices = tree.query_ball_point(node, max_distance)
+        for i in indices:
+            m = nodes[i]
+            if m != node and min_distance <= manhattan_distance(node, m) <= max_distance:
+                cheats.add((node, m))
 
     return cheats
 
 
-def time_saved(nodes, distance_to_end, cheat):
-    (x1, y1), (x2, y2) = cheat
+def get_time_counts(distances, cheats):
+    times = (time_saved(distances, cheat) for cheat in cheats)
 
-    def get_best_time(srcs, dst):
-        src_times = [distance_to_end[s] for s in srcs]
-        dst_time = distance_to_end[dst]
-        diffs = {src_time - dst_time - 2 for src_time in src_times}
-        return max(diffs)
+    return Counter(times)
 
-    if (x1, y1) in nodes:
-        # Cheat goes l <- r
-        dst = (x1, y1)
-        srcs = {(x2 + dx, y2 + dy) for dx, dy in DELTA} & nodes
-        return get_best_time(srcs, dst)
-    if (x2, y2) in nodes:
-        # Cheat goes l -> r
-        dst = (x2, y2)
-        srcs = {(x1 + dx, y1 + dy) for dx, dy in DELTA} & nodes
-        return get_best_time(srcs, dst)
+
+def get_results(time_counts):
+    return sum(c for t, c in time_counts.items() if t >= 100)
 
 
 def part_one(data):
-    G, start, end = build_graph(data)
+    G, end = build_graph(data)
 
-    distance_to_end = dict(nx.single_source_shortest_path_length(G, end))
+    distances = dict(nx.single_source_shortest_path_length(G, end))
 
-    nodes = set(G.nodes)
+    cheats = get_cheats(G, 2)
 
-    cheats = get_possible_cheats(data)
+    time_counts = get_time_counts(distances, cheats)
 
-    times = [time_saved(nodes, distance_to_end, cheat) for cheat in cheats]
-
-    counts = Counter(times)
-
-    """
-    ts = sorted(
-        ((c, t) for t, c in counts.items()),
-        key=lambda x: x[1]
-    )
-
-    for count, time in ts:
-        print(f"There are {count} cheats that save {time} time")
-    """
-
-    return sum(c for t, c in counts.items() if t >= 100)
+    return get_results(time_counts)
 
 
 def part_two(data):
-    return 0
+    G, end = build_graph(data)
+
+    distances = dict(nx.single_source_shortest_path_length(G, end))
+
+    cheats = get_cheats(G, 20)
+
+    time_counts = get_time_counts(distances, cheats)
+
+    return get_results(time_counts)
 
 
 def main():
-    data = """###############
-#...#...#.....#
-#.#.#.#.#.###.#
-#S#...#.#.#...#
-#######.#.#.###
-#######.#.#...#
-#######.#.###.#
-###..E#...#...#
-###.#######.###
-#...###...#...#
-#.#####.#.###.#
-#.#...#.#.#...#
-#.#.#.#.#.#.###
-#...#...#...###
-###############"""
     data = get_data(day=20, year=2024)
     one = part_one(data)
     two = part_two(data)
